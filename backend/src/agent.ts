f5171301ac657bbae04004a669d8051e708f7c5c
vicenteprogrammer <vicentejunior4522@gmail.com>
2025-10-16 15:41:33 -0300

    integrated gemini AI

--- backend/src/agent.ts
+++ backend/src/agent.ts
@@ -1,6 +1,19 @@
 import type { GymPlanRequest } from "./types";
+import fs from 'fs';
+import { GoogleGenAI } from "@google/genai";
+
+const apiKey = process.env.API_KEY;
+
+if (!apiKey) {
+    throw new Error('API_KEY environment variable is required');
+}
+const client = new GoogleGenAI({
+    apiKey
+});
 
 export async function generateGymPlan(input:GymPlanRequest) {
-    console.log(input);
-    return input;
+    const dataTec = fs.readFileSync('knowledge/gym-ai-diretrizes.md', 'utf-8');
+    const response = await client.models.generateContent({model: 'gemini-2.5-flash',contents: "Explain how AI works in a few words"})
+    console.log(response.text);
+    return 'OK'
 }
